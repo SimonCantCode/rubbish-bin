bacd9fcf6480654e3e461c8248a3d13c89e5a2ff
21side <simondepomian@gmail.com>
2022-02-18 10:50:34 +0000

    fixed so it can update numbers to website

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #!/bin/env python3
+# Make this execute on boot
 from time import sleep
 import RPi.GPIO as GPIO
-from supersonic.py import Supersonic # Supersonic uses GPIO.BCM
+from supersonic import Supersonic # Supersonic uses GPIO.BCM
 import requests
 
 # Height is used to compare how full the bin is
@@ -12,10 +13,8 @@ def uppdate(status, distance):
     # Website url and password are located in files hidden with .gitignore
     url = open("url.txt", "r")
     password = open("password.txt", "r")
-
-    _data = {'status': status, 'distance': distance, 'password': password.read()}
-    x = requests.post(url.read(), data=_data)
-    return x.text
+    _data = {'status': status, 'distance': distance, 'password': password.read().replace('\n', '')}
+    requests.post(url.read().replace('\n', ''), data=_data)
 
 #set GPIO Pins
 #GPIO_TRIGGER = 18
@@ -29,7 +28,7 @@ if __name__ == '__main__':
         while True:
             dist = sensor1.distance()
             if dist < rubbish_bin_height and dist > 0:
-                print ("Distance to garbage = {} cm".format(dist))
+                print("Distance to garbage = {} cm".format(dist))
                 uppdate(str((dist/rubbish_bin_height)*100) + "%", dist)
                 sleep(1)
  
